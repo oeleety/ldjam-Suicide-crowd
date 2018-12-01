69ee4265095545cd74626249687f1518738d0c57
Dmitry Matrohin <dima.Woogy@yandex.ru>
2018-12-01 16:36:33 +0300

    Add crowd

---
+++ game/crowd.lua
@@ -0,0 +1,56 @@
+local M = {}
+
+M.items = {}
+local force = 2000
+
+local function moveItem(item, force)
+    if love.keyboard.isDown('w') then item.body:applyForce(0, -force) end
+    if love.keyboard.isDown('a') then item.body:applyForce(-force, 0) end
+    if love.keyboard.isDown('s') then item.body:applyForce(0, force) end
+    if love.keyboard.isDown('d') then item.body:applyForce(force, 0) end
+end
+
+local function createItem(x, y)
+    local res = {}
+    res.body = love.physics.newBody(M.world, x, y, "dynamic")
+    res.body:setFixedRotation(true)
+    res.shape = love.physics.newRectangleShape(10, 15)
+    res.fixture = love.physics.newFixture(res.body, res.shape, 0.1)
+    function res.draw()
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.polygon("fill", res.body:getWorldPoints(res.shape:getPoints())) 
+    end
+    function res.update(dt)
+        moveItem(res, force * dt)
+    end
+    return res;
+end
+
+local function addItem(item)
+    item.fixture:setUserData(M.nextItemNum)
+    M.items[M.nextItemNum] = item 
+    M.nextItemNum = M.nextItemNum + 1
+end
+
+function M.create(world, count)
+    M.world = world
+    M.nextItemNum = 0
+    for i = 1, count do
+        x, y = love.math.random(-10, 10), love.math.random(-20, 20)
+        addItem(createItem(config.windowWidth/2 + x, config.windowHeight/2 + y))
+    end
+end
+
+function M.update(dt)
+    for _, v in pairs(M.items) do
+        v.update(dt)
+    end
+end
+
+function M.draw()
+    for _, v in pairs(M.items) do
+        v.draw()
+    end
+end
+
+return M
--- game/game.lua
+++ game/game.lua
@@ -1,3 +1,24 @@
 local M = {}
 
+function M.load(settings)
+    M.world = love.physics.newWorld(0, 0, true)
+    M.objects = {
+        crowd = require("game/crowd")
+    }
+    M.objects.crowd.create(M.world, 20)
+end
+
+function M.update(dt)
+    M.world:update(dt)
+    for k, v in pairs(M.objects) do
+        v.update(dt)
+    end
+end
+
+function M.draw()
+    for k, v in pairs(M.objects) do
+        v.draw()
+    end
+end
+
 return M
